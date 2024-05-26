2c52b5de1ae3baa0f18ec6814d62218cd49d40d0
mzainxo <120658271+mzainxo@users.noreply.github.com>
2024-05-26 22:39:21 +0500

    Initial Commit with basic UI, face detection, and nav bar designed

---
+++ app/src/main/java/com/example/dontblinkappx/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.dontblinkappx;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.dontblinkappx.databinding.ActivityMainBinding;
+import com.example.dontblinkappx.ui.help.HelpFragment;
+import com.example.dontblinkappx.ui.leaderboard.LeaderboardFragment;
+import com.example.dontblinkappx.ui.play.PlayFragment;
+import com.example.dontblinkappx.ui.statistics.StatisticsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_play) {
+                replaceFragment(new PlayFragment());
+            }
+            else if (itemId == R.id.navigation_leaderboard) {
+                replaceFragment(new LeaderboardFragment());
+            }
+            else if(itemId == R.id.navigation_stats){
+                replaceFragment(new StatisticsFragment());
+            }
+            else if(itemId == R.id.navigation_help){
+                replaceFragment(new HelpFragment());
+            }
+            return true;
+        });
+
+        // Load PlayFragment as the default fragment when the activity starts
+        if (savedInstanceState == null) {
+            replaceFragment(new PlayFragment());
+        }
+        // Set the navigation bar color to match the BottomNavigationView background color
+        getWindow().setNavigationBarColor(
+                getResources().getColor(R.color.dgrey, getTheme())
+        );
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    id("com.chaquo.python") version "14.0.2" apply false
+}
