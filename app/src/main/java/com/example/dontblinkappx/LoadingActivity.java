bef39c286009f4d7f146101b43e31597148fb1d2
mzainxo <120658271+mzainxo@users.noreply.github.com>
2024-06-12 00:14:23 +0500

    loading screen improvements

--- app/src/main/java/com/example/dontblinkappx/LoadingActivity.java
+++ app/src/main/java/com/example/dontblinkappx/LoadingActivity.java
@@ -1,37 +1,42 @@
 package com.example.dontblinkappx;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.bumptech.glide.Glide;
 
 public class LoadingActivity extends AppCompatActivity {
 
     DetectionActivity detectionActivity = new DetectionActivity();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        ImageView imageView = findViewById(R.id.imageView);
+        FullScreenVideoView videoView = findViewById(R.id.videoView);
         TextView loadingText = findViewById(R.id.loadingText);
 
-        // Load GIF using Glide
-        Glide.with(this).asGif().load(R.drawable.minion_load).into(imageView);
+        // Set the video URI and start playback
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.minionvid);
+        videoView.setVideoURI(videoUri);
 
-        // Delay before starting MainActivity (adjust as needed)
+        videoView.setOnPreparedListener(mp -> {
+            mp.setLooping(true); // Loop the video if needed
+            mp.start(); // Start the video
+        });
+
+        // Delay before starting DetectionActivity (adjust as needed)
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                //detectionActivity.shouldRestartGame = true;
                 Intent intent = new Intent(LoadingActivity.this, DetectionActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 4000); // 3000 milliseconds (3 seconds) delay
+        }, 4000); // 4000 milliseconds (4 seconds) delay
     }
 }
